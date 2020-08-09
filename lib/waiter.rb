2434432da42dc116a92459e3d7a3f02b0a9b0dad
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-09 18:06:28 -0400

    all but last method

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
 
+    attr_accessor :name, :yrs_experience
+    @@all =[]
+
+    def initialize(name, yrs_experience)
+        self.name = name
+        self.yrs_experience = yrs_experience
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meals|
+        meals.waiter == self
+        end
+    end
+
+    def best_tipper
+        Customer.all.find do |meals|
+            meals.tip == tip
+        end
+    end
+
 end
